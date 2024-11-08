4038499b2600ea2f7def61bd9f746319873f24fe
潘奕安 <40917898+mercier111@users.noreply.github.com>
2024-11-08 08:23:51 +0800

    exp1

--- datasets/exp1/parse.py
+++ datasets/exp1/parse.py
@@ -1,23 +1,38 @@
-import pandas as pd 
+import os
+import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib as mpl
-
-
+from PIL import Image
+import numpy as np
 mpl.rcParams['font.sans-serif'] = ['SimHei']  # 中文
-plt.rcParams['axes.unicode_minus'] = False    # 负号
+plt.rcParams['axes.unicode_minus'] = False    # 正负号
 
 
-path = 'data/train.txt'
-data = pd.read_csv(path, index_col=0)
+c_path = './cloud/'
+f_path = './forest/'
+
+c_name = [c_path + i for i in os.listdir(c_path)]
+f_name = [f_path + i for i in os.listdir(f_path)]
+
+def show_grey(root=c_name):
+    for n, i in enumerate(c_name):
+        plt.subplot(2, 5, n+1)
+        img = Image.open(i)
+        img = img.convert('L')           #转为灰度图
+        plt.imshow(img)
+        plt.axis('off')
+    plt.show()
+
+def get_mat(root=c_name):
+    for n, i in enumerate(c_name):
+        plt.subplot(2, 5, n+1)
+        img = Image.open(i)
+        img = img.convert('L')            #转为灰度图
+        img = np.array(img)               #转为ndarray
+        yield img
 
 
-color = ['r', 'b']
-for _, i in data.iterrows():
-    x, y, c = i[0], i[1], color[int(i[2])-1]
-    plt.scatter(x, y, color=c)
-plt.xlim(-6, 6)
-plt.ylim(-6, 6)
-plt.show()
+for i in get_mat():
+    print(i)
 
-
-
+show_grey()
